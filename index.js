08e8ce4cdfc43816f9c4d6b8503cbba3e6fb36b3
Sergey Nechaev <snechayev@gmail.com>
2019-03-09 21:54:27 +0300

    Git calls added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const clui = require('clui');
 
-const { listLocalBranches } = require('./src/git');
+const git = require('./src/git');
 
 const run = async () => {
   clear();
@@ -11,8 +12,24 @@ const run = async () => {
     chalk.green(figlet.textSync('prn-lcl', { horizontalLayout: 'full' })),
   );
   console.log('You are about to prune these local branches');
-  const res = await listLocalBranches();
-  console.log('res', res);
+
+  const spinner = new clui.Spinner('Pruning remote branches...');
+  spinner.start();
+
+  await git.remotePruneOrigin();
+  await git.fetchPrune();
+
+  spinner.message('');
+  spinner.stop();
+
+  const localBr = await git.getLocalBranches();
+  console.log('localBr', localBr);
+
+  const remoteBr = await git.getRemoteBranches();
+  console.log('remoteBr', remoteBr);
+
+  const localTrackable = await git.getLocalTrackableBranches();
+  console.log('localTrackable', localTrackable);
 };
 
-run();
+run().catch(err => console.error('An error occured:', err));
